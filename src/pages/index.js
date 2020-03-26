72a72f423ad9b8c0dd4b096456a3fdd3baf83f09
Darren Hebner <darren.hebner@shopify.com>
2020-03-26 13:59:57 -0400

    Adds basic footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,11 @@ import React from "react"
 import Layout from "components/common/Layout"
 import Schedule from "staticContainers/Schedule"
 import SEO from "components/common/SEO"
-import Header from "components/theme/Header"
 import Landing from "staticContainers/Landing"
 
 export default () => (
   <Layout>
     <SEO title="Home" />
-    <Header />
     <Landing />
     <Schedule />
   </Layout>
