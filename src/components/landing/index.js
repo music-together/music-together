4568e42168ac0e317d7c2f68d4ac5932b372aff4
wcgray <cam@tinsoldiersoftware.com>
2020-03-26 10:04:37 -0400

    Schedule UI

---
+++ src/components/landing/index.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import styled from 'styled-components'
+import Header from './header'
+import Schedule from './schedule'
+
+const LandingContainer = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  background-color: black;
+  background-image: url('./schedule-background.png');
+  background-repeat: no-repeat;
+  background-position: top center;
+  position: relative;
+`
+
+const HeroBlock = styled.div`
+  background-color: transparent;
+  height: 662px;
+  position: relative;
+  @media (max-width: 600px) {
+      height: 20px;
+  }
+`
+
+const HeroContainer = styled.div`
+  display: flex;
+  width: fit-content;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  @media (max-width: 600px) {
+      display: none;
+  }
+`
+
+const HeroPredicateBlock = styled.div`
+  background-color: white;
+  padding: 6px 10px;
+  color: black;
+  text-transform: uppercase;
+  font-family: Arial;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 16.7615px;
+  line-height: 20px;
+  letter-spacing: 0.12em;
+`
+
+const HeroSubjectBlock = styled.div`
+  background-color: transparent;
+  color: white;
+  text-transform: uppercase;
+  margin-left: 18px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: Arial;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 16.7615px;
+  line-height: 20px;
+  letter-spacing: 0.12em;
+`
+
+function LandingPage() {
+  return (
+    <LandingContainer>
+      <Header/>
+      <HeroBlock>
+        <HeroContainer>
+          <HeroPredicateBlock>Enjoy</HeroPredicateBlock>
+          <HeroSubjectBlock>Local Musicians</HeroSubjectBlock>
+        </HeroContainer>
+      </HeroBlock>
+      <Schedule/>
+    </LandingContainer>
+  );
+}
+
+export default LandingPage;
