20e39be1d013d61836c9b8222704aa12c52240ca
Darren Hebner <darren.hebner@shopify.com>
2020-03-26 09:47:22 -0400

    Burn all of the extra boilerplate

--- src/components/common/Loaders/Spinner/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Container from "components/common/Container";
-import { Wrapper, Spin } from "./styles";
-
-export default () => (
-  <Wrapper as={Container}>
-    <Spin />
-  </Wrapper>
-);
--- src/components/theme/Header/index.jsx
+++ src/components/theme/Header/index.jsx
@@ -1,31 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
-import Container from "components/common/Container"
-import { Wrapper, Flex, Links } from "./styles"
 
-export default ({ isLoggedIn, logout }) => (
-  <Wrapper>
-    <Flex as={Container}>
-      <Link to="/">
-        <h1>VideoGraphy App</h1>
-      </Link>
-      {isLoggedIn ? (
-        <Links>
-          <Link to="/app/add/">Add new Scene</Link>
-          <Link to="/app/">Scenes</Link>
-          <button
-            type="button"
-            className="btn btn-rounded gradient-green"
-            onClick={logout}
-          >
-            Logout
-          </button>
-        </Links>
-      ) : (
-        <Links>
-          <Link to="/app/">App</Link>
-        </Links>
-      )}
-    </Flex>
-  </Wrapper>
-)
+export default function Header() {
+  return <h1>MusicTogether</h1>
+}
