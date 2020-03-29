c2f66f395882e7c840c41b8c62ab9bd39660f3dd
Chris Daviduik <chris.daviduik@shopify.com>
2020-03-29 12:49:35 -0400

    Link to sponsors from support page. Closes #4

--- src/pages/support.js
+++ src/pages/support.js
@@ -4,6 +4,8 @@ import Layout from "components/common/Layout"
 import SEO from "components/common/SEO"
 import { Heading, Text, NarrowContainer } from "components"
 import {DonateButton} from "components/DonateButton"
+import { Link } from "gatsby"
+import { InlineLinkContainer } from "../components"
 
 const Blurb = styled.div`
   margin: var(--spacing--tight) 0;
@@ -11,11 +13,16 @@ const Blurb = styled.div`
   p {
     margin-bottom: var(--spacing--tight);
   }
+`
 
-  a {
-    display: block;
-    margin: var(--spacing--base) 0;
-  }
+const DonateContainer = styled.div`
+  display: block;
+  margin: var(--spacing--base) 0;
+`
+
+const SupportLink = styled(Link)`
+  display: inline;
+  margin: 0;
 `
 
 export default () => (
@@ -40,15 +47,19 @@ export default () => (
           straight into the pockets of local musicians.
         </Text>
 
-        <DonateButton />
+        <DonateContainer>
+          <DonateButton />
+        </DonateContainer>
 
         <center>
           <Text subdued>Donations are non tax-deductable</Text>
         </center>
 
         <Text>
-          Financial support for MusicTogether is made possible by the generous
-          contributions of our supporters.
+          Financial support for MusicTogether is made possible by the&nbsp;
+          <SupportLink to="/sponsors">
+            <InlineLinkContainer>generous contributions of our supporters.</InlineLinkContainer>
+          </SupportLink>
         </Text>
       </Blurb>
     </NarrowContainer>
