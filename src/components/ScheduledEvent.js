2387b08815dcf348b6c5bb39c988eab2e79b2045
wcgray <cam@tinsoldiersoftware.com>
2020-04-08 01:13:49 -0400

    Try to fix data error in event UI 6

--- src/components/ScheduledEvent.js
+++ src/components/ScheduledEvent.js
@@ -105,7 +105,7 @@ export const ScheduledEvent = ({ event, artists }) => {
   return (
     <ScheduledEventContainer>
       {artistImageUrl && (
-        <ArtistThumbnailContainer>
+        <ArtistThumbnailContainer key={Date.now()}>
           <Thumbnail key={Date.now()} alt={artistImageAlt} src={artistImageUrl} />
         </ArtistThumbnailContainer>
       )}
