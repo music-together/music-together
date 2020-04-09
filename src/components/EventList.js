9b8fbc415cbcb5a7d1e3b5cb5efd69e0823f1706
wcgray <cam@tinsoldiersoftware.com>
2020-04-08 22:22:00 -0400

    Breakup nested loops

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -4,42 +4,55 @@ import { Heading } from "components"
 import { format, parse } from 'date-fns'
 import groupEventsByDay, { DateKeyFormat } from "../utilities/groupEventsByDay"
 
+const DayEvent = ({ artists, event }) => {
+  if (!event.data.Show_time) return null
+
+  const eventArtists = (event.data.Artist || [])
+    .map((artistRecordId) => artists[artistRecordId])
+    .filter((artist) => !!artist)
+
+  if (eventArtists.length === 0) return null
+
+  return (
+    <ScheduledEvent
+      key={Math.random()}
+      artists={eventArtists}
+      event={event}
+    />
+  )
+}
+
+const EventPerDayList = ({ artists, eventData }) => {
+  const { eventsOnDate, eventDate } = eventData;
+
+  return (
+    <div key={eventDate}>
+      <Heading size="large" bold>
+        {format(eventDate, 'eeee MMMM d')}
+      </Heading>
+      {eventsOnDate.map((event) => { return (<DayEvent key={`${eventDate}_${event.recordId}`} artists={artists} event={event}/>) })}
+    </div>
+  )
+}
+
 export const EventList = ({ events, artists }) => {
   const groupedEvents = groupEventsByDay(events);
+  const groupEventKeys = Array.from(groupedEvents.keys());
+
+  const eventsData = [];
+  for (var i = 0;i < groupEventKeys.length;i++) {
+    const eventDateKey = groupEventKeys[i];
+    const eventsOnDate = groupedEvents.get(eventDateKey);
+    const eventDate = parse(eventDateKey, DateKeyFormat, new Date());
+    eventsData.push({ eventDate, eventsOnDate });
+  }
+
+  console.log(`events = ${JSON.stringify(eventsData)}`);
+  console.log(`artists = ${JSON.stringify(artists)}`);
 
   return (
     <>
-      {Array.from(groupedEvents.keys()).map(eventDateKey => {
-        const eventsOnDate = groupedEvents.get(eventDateKey);
-        const eventDate = parse(eventDateKey, DateKeyFormat, new Date());
-
-        return (
-          <div>
-            <Heading size="large" bold>
-              {format(eventDate, 'eeee MMMM d')}
-            </Heading>
-
-            {eventsOnDate.map((event) => {
-              if (!event.data.Show_time) return null
-
-              const eventArtists = (event.data.Artist || [])
-                .map((artistRecordId) => artists[artistRecordId])
-                .filter((artist) => !!artist)
-
-              if (eventArtists.length === 0) return null
-
-              return (
-                <ScheduledEvent
-                  key={event.recordId}
-                  artists={eventArtists}
-                  event={event}
-                />
-              )
-            })}
-          </div>
-        )
-      })
-      }
+      {eventsData.map(eventData => { return (<EventPerDayList key={Math.random()} artists={artists} eventData={eventData}/>) })}
     </>
   )
 }
